1fc81038053685061838a9f7db06bb7bdd6b2078
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2024-09-14 23:04:36 +0530

    database succesfully connected

--- client/src/FrontPage.js
+++ client/src/FrontPage.js
@@ -1,19 +1,62 @@
-import Header from "./Components/Header/Header";
-import ScrollBar from "./Components/Scroll-Bar/ScrollBar";
-import Home from "./Components/Container/Home";
-import Title from "./Components/Title/Title";
-import Sorting from "./Components/Sorting/Sorting";
+// import Header from "./Components/Header/Header";
+// import ScrollBar from "./Components/Scroll-Bar/ScrollBar";
+// import Home from "./Components/Container/Home";
+// import Title from "./Components/Title/Title";
+// import Sorting from "./Components/Sorting/Sorting";
 
-function FrontPage() {
+// function FrontPage() {
+//   return (
+//     <div>
+//       <Header />
+//       <ScrollBar />
+//       <Title />
+//       <Sorting />
+//       <Home />
+//     </div>
+//   );
+// }
+
+// export default FrontPage;
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/user")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <Header />
-      <ScrollBar />
-      <Title />
-      <Sorting />
-      <Home />
+    <div className="App">
+      <h1>Users List</h1>
+      {users.length > 0 ? (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users found.</p>
+      )}
     </div>
   );
 }
 
-export default FrontPage;
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,45 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mY$pQL!58gN#9cD^7sZl",
+  database: "Swiggy",
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    return;
+  }
+  console.log("Connected to the database.");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Express!");
+});
+
+app.get("/user", (req, res) => {
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.listen(PORT, () => {
